b7bc078305a3f4b2e06ea327a54eff4ab8506584
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-01-06 16:27:06 +0100

    Add Link class and types

--- src/Logic/LG.hs
+++ src/Logic/LG.hs
@@ -5,6 +5,55 @@ module Logic.LG where
     You can help by expanding it.
 -}
 
+data LinkType = Tensor | Cotensor
+
+class Link a where
+    linkType :: a -> LinkType
+    premises :: a -> [Node]
+    conclusions :: a -> [Node]
+    mainFormula :: a -> Node
+    mainFormula = main
+
+data FusionTensor = FusionTensor {
+    left :: Node,
+    right :: Node,
+    bottom :: Node,
+    main :: Node }
+instance Link FusionTensor where
+    linkType fut = Tensor
+    premises fut = [left fut, right fut]
+    conclusions fut = [bottom fut]
+
+data FusionCotensor = FusionCotensor {
+    left :: Node,
+    right :: Node,
+    top :: Node,
+    main :: Node }
+instance Link FusionCotensor where
+    linkType fuc = Cotensor
+    conclusions fuc = [left fuc, right fuc]
+    premises fuc = [top fuc]
+
+data FissionTensor = FissionTensor {
+    left :: Node,
+    right :: Node,
+    top :: Node,
+    main :: Node }
+instance Link FissionTensor where
+    linkType fit = Tensor
+    conclusions fit = [left fit, right fit]
+    premises fit = [top fit]
+
+data FissionCotensor = FissionCotensor {
+    left :: Node,
+    right :: Node,
+    bottom :: Node,
+    main :: Node }
+instance Link FissionCotensor where
+    linkType fic = Cotensor
+    premises fic = [left fic, right fic]
+    conclusions fic = [bottom fic]
+
 class Formula a where
     -- common functions on formulas
     -- (obviously, this should actually be in the Framework module)
