0371d106345e3dd57947d9ac95ad348e5c9f5e1b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-01-16 16:03:20 +0100

    Another attempt at term substitution

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -18,16 +18,40 @@ data Subnet = Subnet { context       :: CompositionGraph
 data SubnetGraph = Map.Map Identifier Subnet  -- in which subnet is this node?
 
 substituteValue :: ValueTerm -> ValueTerm -> Term -> Term
-substituteValue (Vv (Variable s)) expansion (V (Vv (Variable s))) = V expansion
-substituteValue var expansion (V (Vv (a :<×> b))) = V (Vv (subVA :<×> subVB))
-substituteValue var expansion (V (Vv (a :<\> b))) = V (Vv (subEA :<\> subVB))
-substituteValue var expansion (V (Vv (a :</> b))) = V (Vv (subVA :</> subEB))
-substituteValue var expansion (V (Mu s a)) = V (Mu s subCA)
-substituteValue var expansion unmodified@(E (Ee (Covariable _))) = unmodified
-substituteValue var expansion (E (Ee (a :<+> b))) = E (Ee (subEA :<+> subEB))
-substituteValue var expansion (E (Ee (a :\ b))) = E (Ee (subVA :\ subEB))
-substituteValue var expansion (E (Ee (a :/ b))) = E (Ee (subEA :/ subVB))
-substituteValue var expansion (E (Comu s a)) = E (Comu s subCA)
-substituteValue var expansion (C (Cut s t u a)) = (C (Cut s t u subCA))
-substituteValue var expansion (C (s :⌈ a)) = (C (s :⌈ subEA))
-substituteValue var expansion (C (a :⌉ s)) = (C (subVA :⌉ s))
+substituteValue a b (V t) = V $ substituteValueV a b t
+substituteValue a b (E t) = E $ substituteValueE a b t
+substituteValue a b (C t) = C $ substituteValueC a b t
+
+substituteValueV :: ValueTerm -> ValueTerm -> ValueTerm -> ValueTerm
+substituteValueV (Vv (Variable s)) expansion (Vv (Variable s)) = expansion
+substituteValueV var expansion (Vv (a :<×> b)) = Vv (subV a :<×> subV b)
+  where subV = substituteValueV var expansion
+substituteValueV var expansion (Vv (a :<\> b)) = Vv (subE a :<\> subV b)
+  where subV = substituteValueV var expansion
+        subE = substituteValueE var expansion
+substituteValueV var expansion (Vv (a :</> b)) = Vv (subV a :</> subE b)
+  where subV = substituteValueV var expansion
+        subE = substituteValueE var expansion
+substituteValueV var expansion (Mu s a) = Mu s subC a
+  where subC = substituteValueC var expansion
+
+substituteValueE :: ValueTerm -> ValueTerm -> ContextTerm -> ContextTerm
+substituteValueE var expansion unmodified@(Ee (Covariable _)) = unmodified
+substituteValueE var expansion (Ee (a :<+> b)) = Ee (subE a :<+> subE b)
+  where subE = substituteValueE var expansion
+substituteValueE var expansion (Ee (a :\ b)) = Ee (subV a :\ subE b)
+  where subV = substituteValueV var expansion
+        subE = substituteValueE var expansion
+substituteValueE var expansion (Ee (a :/ b)) = Ee (subE a :/ subV b)
+  where subV = substituteValueV var expansion
+        subE = substituteValueE var expansion
+substituteValueE var expansion (Comu s a) = Comu s subC a
+  where subC = substituteValueC var expansion
+
+substituteValueC :: ValueTerm -> ValueTerm -> CommandTerm -> CommandTerm
+substituteValueC var expansion (Cut s t u a) = (Cut s t u subC a)
+  where subC = substituteValueC var expansion
+substituteValueC var expansion (s :⌈ a) = (s :⌈ subE a)
+  where subE = substituteValueE var expansion
+substituteValueC var expansion (a :⌉ s) = (subV a :⌉ s)
+  where subV = substituteValueV var expansion
