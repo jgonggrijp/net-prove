f1c04c95f265615b56e9f04feac4a76656189549
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-01-16 14:21:15 +0100

    Try variable substitution

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -16,3 +16,18 @@ data Subnet = Subnet { context       :: CompositionGraph
             deriving (Eq, Show)
 
 data SubnetGraph = Map.Map Identifier Subnet  -- in which subnet is this node?
+
+substituteValue :: ValueTerm -> ValueTerm -> Term -> Term
+substituteValue (Vv (Variable s)) expansion (V (Vv (Variable s))) = V expansion
+substituteValue var expansion (V (Vv (a :<×> b))) = V (Vv (subVA :<×> subVB))
+substituteValue var expansion (V (Vv (a :<\> b))) = V (Vv (subEA :<\> subVB))
+substituteValue var expansion (V (Vv (a :</> b))) = V (Vv (subVA :</> subEB))
+substituteValue var expansion (V (Mu s a)) = V (Mu s subCA)
+substituteValue var expansion unmodified@(E (Ee (Covariable _))) = unmodified
+substituteValue var expansion (E (Ee (a :<+> b))) = E (Ee (subEA :<+> subEB))
+substituteValue var expansion (E (Ee (a :\ b))) = E (Ee (subVA :\ subEB))
+substituteValue var expansion (E (Ee (a :/ b))) = E (Ee (subEA :/ subVB))
+substituteValue var expansion (E (Comu s a)) = E (Comu s subCA)
+substituteValue var expansion (C (Cut s t u a)) = (C (Cut s t u subCA))
+substituteValue var expansion (C (s :⌈ a)) = (C (s :⌈ subEA))
+substituteValue var expansion (C (a :⌉ s)) = (C (subVA :⌉ s))
