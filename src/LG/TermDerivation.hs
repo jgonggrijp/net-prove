a39a55097d064bfb74445a7a15657127c1b0b4ff
Julian Gonggrijp <j.gonggrijp@gmail.com>
2015-01-17 00:46:39 +0100

    Start implementing Subnet extraction

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -70,3 +70,24 @@ data Subnet = Subnet { nodes         :: Set.Set Identifier
 
 type SubnetGraph = Map.Map Identifier Subnet  -- in which subnet is this node?
 
+data ExtractionProgress = Progress { graph        :: CompositionGraph
+                                   , nodesVisited :: Set.Set Identifier
+                                   , subnets      :: [Subnet]
+                                   , subnetGraph  :: SubnetGraph
+                                   }
+
+extractStart = Progress Set.empty [] Map.empty
+
+extractSubnets :: CompositionGraph -> ([Subnet], SubnetGraph)
+extractSubnets graph = subnets, subsGraph
+  where (Progress _ subnets subsGraph) = Map.foldrWithKey extractSubnets' extractStart graph
+
+extractSubnets' :: Identifier -> NodeInfo -> ExtractionProgress -> ExtractionProgress
+extractSubnets' index node progress | Set.member index visited = progress
+                                    | otherwise                = progress'
+  where (Progress graph visited subs subsGraph) = progress
+        progress' = Progress graph visited' newsub:subs subsGraph'
+        newsub = expandSubnet seed (index :@ node) graph  -- use maybe on links instead
+        seed = Subnet (Set.singleton index) (term node) [] [] []
+        visited' = union visited (nodes newsub)
+        subsGraph' = Set.foldr (flip Map.insert newsub) subsGraph (nodes newsub)
