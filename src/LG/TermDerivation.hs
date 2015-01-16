645297f84c52fa638c6105935567716f64b006ac
Julian Gonggrijp <j.gonggrijp@gmail.com>
2015-01-16 21:56:57 +0100

    Next attempt at term substitution

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -17,23 +17,65 @@ data Subnet = Subnet { context       :: CompositionGraph
 
 type SubnetGraph = Map.Map Identifier Subnet  -- in which subnet is this node?
 
--- replace x by y in z
-substitute :: s -> s -> t -> t
-substitute x y (V z)         = V $ substitute x y z
-substitute x y (E z)         = E $ substitute x y z
-substitute x y (C z)         = C $ substitute x y z
-substitute (Vv (Variable s))   y z@(Vv (Variable t))   = if s == t then y else z
-substitute (Ee (Covariable s)) y z@(Ee (Covariable t)) = if s == t then y else z
-substitute x y (Vv z)        = Vv $ substitute x y z
-substitute x y (Ee z)        = Ee $ substitute x y z
-substitute x y (Mu s z)      = Mu s $ substitute x y z
-substitute x y (Comu s z)    = Comu s $ substitute x y z
-substitute x y (v :<×> w)    = substitute x y v :<×> substitute x y w
-substitute x y (v :<\> w)    = substitute x y v :<\> substitute x y w
-substitute x y (v :</> w)    = substitute x y v :</> substitute x y w
-substitute x y (v  :\  w)    = substitute x y v  :\  substitute x y w
-substitute x y (v  :/  w)    = substitute x y v  :/  substitute x y w
-substitute x y (v :<+> w)    = substitute x y v :<+> substitute x y w
-substitute x y (Cut s t u z) = Cut s t u $ substitute x y z
-substitute x y (z :⌈ s)      = substitute x y z :⌈ s
-substitute x y (s :⌉ z)      = s :⌉ substitute x y z
+class Substitutable t1 t2 where
+    substitute :: t1 -> t1 -> t2 -> t2  -- substitute x for y in z
+
+instance Substitutable ValueTerm Term where
+    substitute x y (V z) = V $ substitute x y z
+    substitute x y (E z) = E $ substitute x y z
+    substitute x y (C z) = C $ substitute x y z
+
+instance Substitutable ContextTerm Term where
+    substitute x y (V z) = V $ substitute x y z
+    substitute x y (E z) = E $ substitute x y z
+    substitute x y (C z) = C $ substitute x y z
+
+instance Substitutable ValueTerm ValueTerm where
+    substitute x (Vv (Variable s)) z@(Vv (Variable t)) = if s == t then x else z
+    substitute x y (Vv z) = Vv $ substitute x y z
+    substitute x y (Mu s z) = Mu s $ substitute x y z
+
+instance Substitutable ValueTerm ContextTerm where
+    substitute x y (Ee z) = Ee $ substitute x y z
+    substitute x y (Comu s z) = Comu s $ substitute x y z
+
+instance Substitutable ValueTerm CommandTerm where
+    substitute x y (Cut s t u z) = Cut s t u $ substitute x y z
+    substitute x y (z :⌈ s) = substitute x y z :⌈ s
+    substitute x y (s :⌉ z) = s :⌉ substitute x y z
+
+instance Substitutable ContextTerm ValueTerm where
+    substitute x y (Vv z) = Vv $ substitute x y z
+    substitute x y (Mu s z) = Mu s $ substitute x y z
+
+instance Substitutable ContextTerm ContextTerm where
+    substitute x (Ee (Covariable s)) z@(Ee (Covariable t)) = if s == t then x else z
+    substitute x y (Ee z) = Ee $ substitute x y z
+    substitute x y (Comu s z) = Comu s $ substitute x y z
+
+instance Substitutable ContextTerm CommandTerm where
+    substitute x y (Cut s t u z) = Cut s t u $ substitute x y z
+    substitute x y (z :⌈ s) = substitute x y z :⌈ s
+    substitute x y (s :⌉ z) = s :⌉ substitute x y z
+
+instance Substitutable ValueTerm ValueTerm' where
+    substitute (Vv x) (Vv (Variable s)) z@(Variable t) = if s == t then x else z
+    substitute x y (v :<×> w) = substitute x y v :<×> substitute x y w
+    substitute x y (v :<\> w) = substitute x y v :<\> substitute x y w
+    substitute x y (v :</> w) = substitute x y v :</> substitute x y w
+
+instance Substitutable ValueTerm ContextTerm' where
+    substitute x y (v  :\  w) = substitute x y v  :\  substitute x y w
+    substitute x y (v  :/  w) = substitute x y v  :/  substitute x y w
+    substitute x y (v :<+> w) = substitute x y v :<+> substitute x y w
+
+instance Substitutable ContextTerm ValueTerm' where
+    substitute x y (v :<×> w) = substitute x y v :<×> substitute x y w
+    substitute x y (v :<\> w) = substitute x y v :<\> substitute x y w
+    substitute x y (v :</> w) = substitute x y v :</> substitute x y w
+
+instance Substitutable ContextTerm ContextTerm' where
+    substitute (Ee x) (Ee (Covariable s)) z@(Covariable t) = if s == t then x else z
+    substitute x y (v  :\  w) = substitute x y v  :\  substitute x y w
+    substitute x y (v  :/  w) = substitute x y v  :/  substitute x y w
+    substitute x y (v :<+> w) = substitute x y v :<+> substitute x y w
