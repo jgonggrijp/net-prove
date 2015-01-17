0352946d16d75b53e4abefc96ba11651400f9444
Julian Gonggrijp <j.gonggrijp@gmail.com>
2015-01-17 11:23:53 +0100

    Fix semantics for term substitution

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -4,53 +4,64 @@ import LG.Graph
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
-data Subnet a = Subnet { context       :: CompositionGraph
-                       , fringe        :: Set.Set Identifier
-                       , term          :: a
-                       , commandLinks  :: [Link]  -- followable only
-                       , cotensorLinks :: [Link]  -- same
-                       , muLinks       :: [Link]  -- same
-                       }
-              deriving (Eq, Show)
+data Subnet = Subnet { context       :: CompositionGraph
+                     , fringe        :: Set.Set Identifier
+                     , term          :: Term
+                     , commandLinks  :: [Link]  -- followable only
+                     , cotensorLinks :: [Link]  -- same
+                     , muLinks       :: [Link]  -- same
+                     }
+            deriving (Eq, Show)
 
 type SubnetGraph a = Map.Map Identifier (Subnet a)  -- in which subnet is this node?
 
-class Term a where
-  substitute :: Term b => b -> b -> a -> a --substitute x for y in z
-  asValue    :: a -> Maybe ValueTerm'
-  asContext  :: a -> Maybe ContextTerm'
-  asValue   _ = Nothing
-  asContext _ = Nothing
+class Substitutable a where
+    substitute :: ValidSubstitution b => b -> b -> a -> a
+    --substitute x for y in z
 
-instance Term CommandTerm where
-  substitute x y (Cut s t u z) = Cut s t u $ substitute x y z
-  substitute x y (z :⌈ s)      = substitute x y z :⌈ s
-  substitute x y (s :⌉ z)      = s :⌉ substitute x y z
+class ValidSubstitution a where
+    asValue    :: a -> Maybe ValueTerm
+    asContext  :: a -> Maybe ContextTerm
+    asValue   _ = Nothing
+    asContext _ = Nothing
 
-instance Term ContextTerm where
-  asContext (Ee x) = asContext x
-  substitute x y (Comu s z) = Comu s $ substitute x y z
-  substitute x y (Ee z)     = Ee     $ substitute x y z
+instance ValidSubstitution ValueTerm where
+    asValue x = Just x
 
-instance Term ContextTerm' where
-  asContext x@(Covariable _) = Just x
-  substitute x y (v  :\  w)       = substitute x y v  :\  substitute x y w
-  substitute x y (v  :/  w)       = substitute x y v  :/  substitute x y w
-  substitute x y (v :<+> w)       = substitute x y v :<+> substitute x y w
-  substitute x y z@(Covariable _) = case (asContext x, asContext y) of
-    (Just x', Just y') -> if y' == z then x' else z
-    _ -> z
+instance ValidSubstitution ContextTerm where
+    asContext x = Just x
 
-instance Term ValueTerm where
-  asValue (Vv x) = asValue x
-  substitute x y (Mu s z) = Mu s $ substitute x y z
-  substitute x y (Vv z)   = Vv   $ substitute x y z
+instance Substitutable CommandTerm where
+    substitute x y (Cut s t u z) = Cut s t u $ substitute x y z
+    substitute x y (z :⌈ s)      = substitute x y z :⌈ s
+    substitute x y (s :⌉ z)      = s :⌉ substitute x y z
 
-instance Term ValueTerm' where
-  asValue x@(Variable _) = Just x
-  substitute x y (v :<×> w) = substitute x y v :<×> substitute x y w
-  substitute x y (v :<\> w) = substitute x y v :<\> substitute x y w
-  substitute x y (v :</> w) = substitute x y v :</> substitute x y w
-  substitute x y z@(Variable _) = case (asValue x, asValue y) of
-    (Just x', Just y') -> if y' == z then x' else z
-    _ -> z
+instance Substitutable ContextTerm where
+    substitute x y z@(Ee (Covariable s)) = case (asContext x, asContext y) of
+        (Just _, Just (Ee (Covariable t))) -> if s == t then x else z
+        _ -> z
+    substitute x y (Ee z)                = Ee     $ substitute x y z
+    substitute x y (Comu s z)            = Comu s $ substitute x y z
+
+instance Substitutable ContextTerm' where
+    substitute x y (v  :\  w)       = substitute x y v  :\  substitute x y w
+    substitute x y (v  :/  w)       = substitute x y v  :/  substitute x y w
+    substitute x y (v :<+> w)       = substitute x y v :<+> substitute x y w
+    substitute x y z@(Covariable s) = case (asContext x, asContext y) of
+        (Just (Ee x'), Just (Ee (Covariable t))) -> if s == t then x' else z
+        _ -> z
+
+instance Substitutable ValueTerm where
+    substitute x y z@(Vv (Variable s)) = case (asValue x, asValue y) of
+        (Just _, Just (Vv (Variable t))) -> if s == t then x else z
+        _ -> z
+    substitute x y (Mu s z)            = Mu s $ substitute x y z
+    substitute x y (Vv z)              = Vv   $ substitute x y z
+
+instance Substitutable ValueTerm' where
+    substitute x y (v :<×> w)     = substitute x y v :<×> substitute x y w
+    substitute x y (v :<\> w)     = substitute x y v :<\> substitute x y w
+    substitute x y (v :</> w)     = substitute x y v :</> substitute x y w
+    substitute x y z@(Variable s) = case (asValue x, asValue y) of
+        (Just (Vv x'), Just (Vv (Variable t))) -> if s == t then x' else z
+        _ -> z
