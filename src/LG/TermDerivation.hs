fa08f0d1fd39feed73cec0431d1355e0e4215f83
Julian Gonggrijp <j.gonggrijp@gmail.com>
2015-01-18 17:42:57 +0100

    Finish definition of extractSubnets'

--- src/LG/TermDerivation.hs
+++ src/LG/TermDerivation.hs
@@ -2,6 +2,7 @@ module LG.TermDerivation where
 
 import LG.Graph
 import LG.Term
+import Data.Maybe
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
@@ -25,14 +26,17 @@ extractStart = Progress Set.empty [] Map.empty
 
 extractSubnets :: CompositionGraph -> ([Subnet], SubnetGraph)
 extractSubnets graph = subnets, subsGraph
-  where (Progress _ subnets subsGraph) = Map.foldrWithKey extractSubnets' extractStart graph
+  where (Progress _ _ subnets subsGraph) = Map.foldrWithKey extractSubnets' extractStart graph
 
 extractSubnets' :: Identifier -> NodeInfo -> ExtractionProgress -> ExtractionProgress
 extractSubnets' index node progress | Set.member index visited = progress
                                     | otherwise                = progress'
   where (Progress graph visited subs subsGraph) = progress
-        progress' = Progress graph visited' newsub:subs subsGraph'
-        newsub = expandSubnet seed (index :@ node) graph  -- use maybe on links instead
         seed = Subnet (Set.singleton index) (term node) [] [] []
+        expand s = expandSubnet s graph (index :@ node)  -- note: partial appl.
+        newsub' = maybe seed (expand seed) $ premiseOf node
+        newsub = maybe newsub' (expand newsub') $ succedentOf node
         visited' = union visited (nodes newsub)
         subsGraph' = Set.foldr (flip Map.insert newsub) subsGraph (nodes newsub)
+        progress' = Progress graph visited' newsub:subs subsGraph'
+
