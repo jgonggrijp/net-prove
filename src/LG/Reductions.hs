c75536aaa5ddc0d553263a422d3c29ccdfcce7e8
cowl <cowl@users.noreply.github.com>
2015-01-19 18:41:19 +0100

    Pretty definitions of contractions and interactions

--- src/LG/Reductions.hs
+++ src/LG/Reductions.hs
@@ -3,8 +3,50 @@ module LG.Reductions where
 import LG.Graph
 import qualified Data.Map as Map
 
+data ProofTransformation = [Link] :⤳ [Link]
+contraction              = (:⤳ [])
+interaction              = ([[ Active 1, Active 2 ] :○: [ Active 0 ],
+                             [ Active 0 ] :○: [ Active 3, Active 4 ]] :⤳)
+
+-- Proof net transformations (Moortgat & Moot 2012, pp 9-10)
+contractions = [rdivR, prodL, ldivR, rdifL, cprdR, ldifL]
+interactions = [g1, g3, g2, g4]
+
+rdivR = contraction [[ Active 1, Active 2 ] :○: [ Active 3 ], -- R/
+                     [ Active 3 ] :●: [ MainT  4, Active 2 ]]
+
+prodL = contraction [[ MainT  1 ] :●: [ Active 2, Active 3 ], -- L<×>
+                     [ Active 2, Active 3 ] :○: [ Active 4 ]]
+
+ldivR = contraction [[ Active 2, Active 1 ] :○: [ Active 3 ], -- R\
+                     [ Active 3 ] :●: [ Active 2, MainT  4 ]]
+
+rdifL = contraction [[ MainT  1, Active 2 ] :●: [ Active 3 ], -- L</>
+                     [ Active 3 ] :○: [ Active 4, Active 2 ]]
+
+cprdR = contraction [[ Active 1 ] :○: [ Active 2, Active 3 ], -- R<+>
+                     [ Active 2, Active 3 ] :●: [ MainT  4 ]]
+
+ldifL = contraction [[ Active 2, MainT  1 ] :●: [ Active 3 ], -- L<\>
+                     [ Active 3 ] :○: [ Active 2, Active 4 ]]
+
+g1    = interaction [[ Active 1 ] :○: [ Active 3, Active 0 ], -- Associativity
+                     [ Active 0, Active 2 ] :○: [ Active 4 ]]
+
+g3    = interaction [[ Active 1, Active 0 ] :○: [ Active 3 ], -- Associativity
+                     [ Active 2 ] :○: [ Active 0, Active 4 ]]
+
+g2    = interaction [[ Active 1 ] :○: [ Active 0, Active 4 ], -- Commutativity
+                     [ Active 0, Active 2 ] :○: [ Active 3 ]]
+
+g4    = interaction [[ Active 2 ] :○: [ Active 3, Active 0 ], -- Commutativity
+                     [ Active 1, Active 0 ] :○: [ Active 4 ]]
+
+--------------------------------------------------------------------------------
 -- Eliminate axiom links so that the composition graph may be interpreted as a
--- proof structure. (Note that this also deletes any unconnected formula!)
+-- proof structure. (Note that this also deletes any unconnected subgraph
+-- consisting of no links or only axiom links!)
+
 reduce :: CompositionGraph -> CompositionGraph
 reduce = Map.mapMaybe (\(Node f t p s) -> case (del p, del s) of
   (Nothing, Nothing) -> Nothing
