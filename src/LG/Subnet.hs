b60b42410302deb2e06e8d0f6f0b2c9555c92e62
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-01-20 11:32:35 +0100

    Make expandNode direction-agnostic and node-aware

--- src/LG/Subnet.hs
+++ src/LG/Subnet.hs
@@ -85,6 +85,8 @@ consumeLink net graph nodeID link@(t1 :|: t2)
 
 expandTentacle' :: Subnet -> Graph -> Tentacle' -> Subnet
 expandTentacle' net graph tentacle' = case tentacle' of
-    (Prem index) -> maybe net (expandUpwards net graph) $ succedentOf node
-    (Succ index) -> maybe net (expandDownwards net graph) $ premiseOf node
+    (Prem index) -> maybe net (expandNode index net graph) $ succedentOf node
+    (Succ index) -> maybe net (expandNode index net graph) $ premiseOf node
   where node = lookup index graph
+
+expandNode :: Identifier -> Subnet -> Graph -> Link -> Subnet
