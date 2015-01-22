d1c3f13f1f87da8b5a902af6ece48f877309bba3
cowl <cowl@users.noreply.github.com>
2015-01-22 02:44:29 +0100

    Connecting and disconnecting links from a graph)

--- src/LG/Reductions.hs
+++ src/LG/Reductions.hs
@@ -46,7 +46,6 @@ g2    = interaction [[ Active 1 ] :○: [ Active 0, Active 4 ], -- Commutativity
 g4    = interaction [[ Active 2 ] :○: [ Active 3, Active 0 ], -- Commutativity
                      [ Active 1, Active 0 ] :○: [ Active 4 ]]
 
-
 --------------------------------------------------------------------------------
 -- Eliminate axiom links so that the composition graph may be interpreted as a
 -- proof structure. (Note that this also deletes any unconnected subgraph
@@ -76,7 +75,7 @@ class Unifiable a where
   unify' x y = unify x y ([],[])
 
 instance Unifiable Int where
-  apply (_, u) x = fromJust $ lookup x u
+  apply (_, u) x           = fromJust $ lookup x u
   unify x y (seen1x, seen) = case lookup x seen of
     Nothing -> Just ((x,y):seen1x, (x,y):seen)
     Just y' -> if y == y'
@@ -84,14 +83,14 @@ instance Unifiable Int where
                else Nothing
 
 instance Unifiable a => Unifiable [a] where
-  apply = fmap . apply
+  apply                 = fmap . apply
   unify (x:xs) (y:ys) u = unify x y u >>= unify xs ys
   unify []     []     u = Just u
   unify _      _      _ = Nothing
 
 instance Unifiable Tentacle where
-  apply u (MainT  x) = MainT  $ apply u x
-  apply u (Active x) = Active $ apply u x
+  apply u (MainT  x)            = MainT  $ apply u x
+  apply u (Active x)            = Active $ apply u x
   unify (MainT  x) (MainT  y) u = unify x y u
   unify (Active x) (Active y) u = unify x y u
   unify _          _          _ = Nothing
@@ -100,34 +99,38 @@ instance Unifiable Link where
   apply u (p :○: s) = apply u p :○: apply u s
   apply u (p :●: s) = apply u p :●: apply u s
   apply u (p :|: s) = apply u p :|: apply u s
-  unify l1 l2 u = case (l1, l2) of
+  unify l1 l2 u     = case (l1, l2) of
     ((p1 :○: s1), (p2 :○: s2)) -> unify s1 s2 u >>= unify p1 p2
     ((p1 :●: s1), (p2 :●: s2)) -> unify s1 s2 u >>= unify p1 p2
     ((p1 :|: s1), (p2 :|: s2)) -> unify s1 s2 u >>= unify p1 p2
     _                          -> Nothing
 
+instance Unifiable ProofTransformation where
+  apply u (p :⤳ s) = apply u p :⤳ apply u s
+  unify (p1 :⤳ s1) (p2 :⤳ s2) u = unify s1 s2 u >>= unify p1 p2
 
--- Give all possible unifications for some set of links such that the links
--- occur in the given graph.
+
+-- Give all possible unifications for some set of links such that all the links
+-- occur in the given graph in conjunction.
 -- To find the unifications of the first link, we simply try to unify with all
 -- links. This assumes that all links have at least one tentacle leading up.
 -- When we have the first set of (tentatively) possible unifications, we expand
 -- (or shrink) this set nondeterministically by attempting to unify at least all
--- 'outer' links (that is, at least all the links that are either a premise or a
--- succedent of nodes that we've only seen once). Because of this, each next
--- link must be in some way connected to one that came before!
+-- 'outer' links (that is, at least all the links that have the nodes that we've
+-- only seen once as a premise or a succedent). Note that this requires each
+-- next link to be in some way connected to one that came before!
 -- Although laziness helps us a lot here, this could be done more efficiently by
 -- already doing it at the individual link unification stage, but the code
 -- would be much uglier, and we never have big subgraphs anyway.
-partialUnify :: [Link] -> CompositionGraph -> [Unification]
-partialUnify []     _ = []
-partialUnify (l1:ls1) g = firsts l1 >>= rest ls1 where
-  firsts link = Map.elems $ Map.mapMaybe (\n -> succedentOf n >>= unify' link) g
+partialUnify :: CompositionGraph -> [Link] -> [Unification]
+partialUnify _ []       = []
+partialUnify g (l1:ls1) = firsts l1 >>= rest ls1 where
+  firsts link = Map.elems $ Map.mapMaybe (\n -> premiseOf n >>= unify' link) g
   rest []     u             = [u]
   rest (l:ls) u@(seen1x, _) = let outerNodes   = map snd seen1x
                                   all' getLink = mapMaybe (getLink . (g Map.!))
-                                  nearLinks    = all' succedentOf outerNodes ++
-                                                 all' premiseOf   outerNodes
+                                  nearLinks    = all' premiseOf   outerNodes ++
+                                                 all' succedentOf outerNodes
                               in  mapMaybe (\l' -> unify l l' u) nearLinks
                                   >>= rest ls
 
@@ -136,3 +139,15 @@ partialUnify (l1:ls1) g = firsts l1 >>= rest ls1 where
 -- Once we have a transformation rule and an applicable unification, we can
 -- actually apply it.
 
+-- Remove/add the succedentOf/premiseOf link references to the nodes of a graph
+connect, disconnect :: CompositionGraph -> [Link] -> CompositionGraph
+connect    = foldl' (update Just)
+disconnect = foldl' (update $ const Nothing)
+
+update :: (Link -> Maybe Link) -> CompositionGraph -> Link -> CompositionGraph
+update r graph link = update' graph (succedents link) (premises link) where
+  updateTop (Node f t p s) = Node f t p (r link)
+  updateBot (Node f t p s) = Node f t (r link) s
+  update' g' (k:ks) s = update' (Map.adjust updateBot k g') ks s
+  update' g' p (k:ks) = update' (Map.adjust updateTop k g') p ks
+  update' g' _      _ = g'
