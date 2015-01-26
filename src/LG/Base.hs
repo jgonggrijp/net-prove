c08cf971e352c40e29a7e120564d3cc3aad86bba
cowl <cowl@users.noreply.github.com>
2015-01-27 00:34:28 +0100

    More succinct Show instances for demonstrative purposes

--- src/LG/Base.hs
+++ src/LG/Base.hs
@@ -3,21 +3,40 @@ module LG.Base where
 type Name = String
 type Identifier = Int
 
-data Occurrence a = Identifier :@ a deriving (Eq, Show)
+data Occurrence a = Identifier :@ a deriving (Eq)
 
 abstract :: Occurrence a -> a
 abstract (_ :@ x) = x
 
-data Formula = P PositiveFormula | N NegativeFormula deriving (Eq, Show)
+data Formula = P PositiveFormula | N NegativeFormula deriving (Eq)
 
 data PositiveFormula = AtomP Name
                      | Formula :<×>: Formula
                      | Formula :<\>: Formula
                      | Formula :</>: Formula
-                     deriving (Eq, Show)
+                     deriving (Eq)
 
 data NegativeFormula = AtomN Name
                      | Formula  :\:  Formula
                      | Formula  :/:  Formula
                      | Formula :<+>: Formula
-                     deriving (Eq, Show)
+                     deriving (Eq)
+
+instance Show a => Show (Occurrence a) where
+  show (k :@ a) = show k ++ "@ " ++ show a
+
+instance Show Formula where
+  show (P f) = show f
+  show (N f) = show f
+
+instance Show PositiveFormula where
+  show (AtomP   f) = show f ++ "⁺"
+  show (f :<×>: e) = "(" ++ show f ++ "❨×❩"  ++ show e ++ ")"
+  show (f :<\>: e) = "(" ++ show f ++ "❨\\❩" ++ show e ++ ")"
+  show (f :</>: e) = "(" ++ show f ++ "❨/❩"  ++ show e ++ ")"
+
+instance Show NegativeFormula where
+  show (AtomN   f) = show f ++ "⁻"
+  show (f :<+>: e) = "(" ++ show f ++ "❨+❩" ++ show e ++ ")"
+  show (f  :\:  e) = "(" ++ show f ++  "\\" ++ show e ++ ")"
+  show (f  :/:  e) = "(" ++ show f ++  "/"  ++ show e ++ ")"
