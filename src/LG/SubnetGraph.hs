d66673e00c96390ef8173118dde9260426160b53
Julian Gonggrijp <j.gonggrijp@gmail.com>
2015-01-26 01:52:13 +0100

    Implement extendCommand and extendCotensor

--- src/LG/SubnetGraph.hs
+++ src/LG/SubnetGraph.hs
@@ -65,9 +65,60 @@ extendMu net sgraph cgraph target link@(t1 :|: t2)
         mus' = Set.delete link mus
         ourNet = Subnet ourNodes muTerm coms cots mus'
         mergeNet = merge ourNet theirNet theirVar
-        update i s | Set.member i (nodes ourNet) = mergeNet
-                   | Set.member i (nodes theirNet) = mergeNet
+        update i s | Set.member i (nodes mergeNet) = mergeNet
                    | otherwise = s
         sgraph' = Map.mapWithKey update sgraph
         finalMu | Set.null mus' = [mergeNet]
                 | otherwise     = []
+
+extendCommand :: Subnet -> SubnetGraph -> CompositionGraph -> Link -> Link -> [Subnet]
+extendCommand net sgraph cgraph target link@(t1 :|: t2) = extensions
+  where (Subnet ourNodes ourTerm coms cots mus) = net
+        (i1, i2) = (referee t1, referee t2)
+        (ourID, theirID) | Set.member i1 ourNodes = (i1, i2)
+                         | Set.member i2 ourNodes = (i2, i1)
+        theirVar = term $ Map.lookup theirID cgraph
+        commandTerm = case (theirVar, ourTerm) of
+            (Va (Variable v), E t) -> C $ v :⌉ t
+            (Ev (Covariable v), V t) -> C $ t :⌈ v
+        coms' = Set.delete link coms
+        mergeNodes = Set.insert theirID ourNodes
+        mergeNet = Subnet mergeNodes commandTerm coms' cots mus
+        update i s | Set.member i mergeNodes = mergeNet
+                   | otherwise = s
+        sgraph' = Map.mapWithKey update sgraph
+        extensions = validExtensions sgraph' cgraph target mergeNet
+
+extendCotensor :: Subnet -> SubnetGraph -> CompositionGraph -> Link -> Link -> [Subnet]
+extendCotensor net sgraph cgraph target link | bothActiveIncluded = extensions
+                                             | otherwise          = []
+  where (Just tm :-: [t1, t2]) = transpose link
+        (Subnet ourNodes ourTerm@(C commandTerm) coms cots mus) = net
+        (im, i1, i2) = (referee' tm, referee' t1, referee' t2)
+        bothActiveIncluded = Set.member i1 ourNodes && Set.member i2 ourNodes
+        termOfHead = term $ flip Map.lookup cgraph $ referee $ head
+        representative t
+            | v@(term $ Map.lookup (referee' t) cgraph) `isSubtermOf` ourTerm = v
+            | otherwise = case t of
+                (Prem i) -> termOfHead $ premises $ fromJust $ succedentOf $ Map.lookup i cgraph
+                (Succ i) -> termOfHead $ succedents $ fromJust $ premiseOf $ Map.lookup i cgraph
+        extractName (Va (Variable v)) = v
+        extractName (Ev (Covariable v)) = v
+        name = extractName . representative
+        ns = (name t1, name t2)
+        (n1, n2) = case link of
+            ([MainT _] :●: [Active _, Active _]) -> ns
+            ([Active _] :●: [MainT _, Active _]) -> ns
+            ([Active _] :●: [Active _, MainT _]) -> swap ns
+            ([MainT _, Active _] :●: [Active _]) -> swap ns
+            ([Active _, MainT _] :●: [Active _]) -> ns
+            ([Active _, Active _] :●: [MainT _]) -> ns
+        nm = extractName $ term $ Map.lookup im cgraph
+        mergeNodes = Set.insert im ourNodes
+        mergeTerm = C $ Cut n1 n2 nm commandTerm
+        cots' = Set.delete link cots
+        mergeNet = Subnet mergeNodes mergeTerm coms cots' mus
+        update i s | Set.member i mergeNodes = mergeNet
+                   | otherwise = s
+        sgraph' = Map.mapWithKey update sgraph
+        extensions = validExtensions sgraph' cgraph target mergeNet
