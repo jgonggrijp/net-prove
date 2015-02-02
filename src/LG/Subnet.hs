fe9e5350863fcef9b019d5b1798c208cdb0492c6
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-02-02 17:26:02 +0100

    Add more comments to LG.Subnet

--- src/LG/Subnet.hs
+++ src/LG/Subnet.hs
@@ -25,22 +25,78 @@ import LG.Base
 import LG.Term
 import LG.Graph
 
+{-
+    A subnet is essentially a connected subset of a CompositionGraph
+    which is a valid proof net with an associated term in its own
+    right. While the "substance" of a subnet is defined by its
+    interior nodes and links, we are more interested in its direct
+    environment of *exterior* links that can be followed towards
+    other subnets (for the purpose of deriving a term for the entire
+    CompositionGraph). Hence, the datastructure below emphasizes
+    those followable exterior links.
+
+    A command link is an axiom link (:|:) of which the upper
+    tentacle is attached to a value node and the lower tentacle is
+    attached to a context node. A (co)mu link is an axiom link of
+    which the tentacles are attached in the opposite way. These types
+    of links have been illustrated in Figure 16 of [MM12].
+
+    The rules to determine whether an exterior link is followable
+    are described partially in definitions (8) of [MM12 p. 16] and
+    partially in the enumeration on [MM12 p. 25]. They are summarized
+    as follows. A command link is followable upwards from the current
+    subnet if the associated formula is negative, or downwards if the
+    formula is positive. A cotensor link is followable if both of its
+    active formulae belong to nodes which are already interior to the
+    current subnet. A mu link is followable upwards from the current
+    subnet if its associated formula is positive, or downwards if the
+    formula is negative.
+-}
 data Subnet = Subnet { nodes         :: Set.Set Identifier
                      , sterm         :: Term
-                     , commandLinks  :: Set.Set Link  -- followable only
-                     , cotensorLinks :: Set.Set Link  -- same
-                     , muLinks       :: Set.Set Link  -- same
+                     , commandLinks  :: Set.Set Link
+                     , cotensorLinks :: Set.Set Link
+                     , muLinks       :: Set.Set Link
                      }
             deriving (Eq, Show)
 
+{-
+    Subnets are built inductively. Trivially, a node in isolation is
+    always a subnet where the subnet term is the term of the node and
+    there are at most two followable exterior links.
+
+    fromNode produces such a trivial single-node subnet, in which
+    the followable links are not yet identified.
+-}
 fromNode :: Occurrence NodeInfo -> Subnet
 fromNode (nodeID :@ nodeInfo) = Subnet onlyNodeID nodeTerm none none none
   where onlyNodeID = Set.singleton nodeID
         nodeTerm = fromNodeTerm (term nodeInfo)
         none = Set.empty
 
--- merge first subnet into second, hooking the former's term into
--- the given (co)variable of the latter's term
+{-
+    There are two types of circumstances under which a subnet can be
+    absorbed into another, adjacent subnet, to produce a larger
+    subnet that includes all nodes and links of both as well as the
+    link that connected them in its interior. In the first type of
+    circumstance, the connecting link is either a tensor or a
+    substitution link (axiom attached to two nodes of the same type)
+    and the followable exterior links are unaffected. Which subnet
+    should be absorbed into which is determined by tentacle
+    direction; main merges into active. In the second type of
+    circumstance, the connecting link is a followable (as defined
+    above) and the subnet from which the connecting link is
+    followable is absorbed into the other. In this case the
+    followable link should be removed from the combined subnet.
+
+    merge as defined below assumes the first type of circumstance,
+    and is used as such in the current module. However, with proper
+    preparation it can also be used for the second type of
+    circumstance and it is used in this way in LG.SubnetGraph.
+
+    net1 is merged into net2, substituting the term of net1 for v of
+    net2.
+-}
 merge :: Subnet -> Subnet -> NodeTerm -> Subnet
 merge net1 net2 v = Subnet allNodes mergeTerm mergeCommand mergeCotensor mergeMu
   where (Subnet nodes1 term1 command1 cotensor1 mu1) = net1
@@ -53,7 +109,7 @@ merge net1 net2 v = Subnet allNodes mergeTerm mergeCommand mergeCotensor mergeMu
         mergeCotensor = Set.union cotensor1 cotensor2
         mergeMu = Set.union mu1 mu2
 
--- test whether the latter falls within the former
+-- Tests whether net1 is a sub-Subnet of net2.
 includes :: Subnet -> Subnet -> Bool
 net2 `includes` net1 = and [incNodes, incTerm, incCommand, incCotensor, incMu]
   where incNodes = (nodes net1) `Set.isSubsetOf` (nodes net2)
