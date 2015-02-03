3c7a09d1bbae7f4ce0740efdc65dcfb3a25cd44a
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-02-03 11:18:36 +0100

    Make subnet extraction in LG.SubnetGraph more elegant

--- src/LG/SubnetGraph.hs
+++ src/LG/SubnetGraph.hs
@@ -12,30 +12,20 @@ import LG.Subnet
 
 type SubnetGraph = Map.Map Identifier Subnet  -- in which subnet is this node?
 
-data ExtractionProgress = Progress { graph        :: CompositionGraph
-                                   , nodesVisited :: Set.Set Identifier
-                                   , subnets      :: [Subnet]
-                                   , subnetGraph  :: SubnetGraph
-                                   }
+type Extraction = ([Subnet], SubnetGraph)
 
-seedProgress :: CompositionGraph -> ExtractionProgress
-seedProgress graph = Progress graph Set.empty [] Map.empty
+extractSubnets :: CompositionGraph -> Extraction
+extractSubnets graph = Map.foldrWithKey extract ([], Map.empty) graph
+  where extract = extractSubnets' graph
 
-extractSubnets :: CompositionGraph -> ([Subnet], SubnetGraph)
-extractSubnets graph = (subnets extractEnd, subnetGraph extractEnd)
-  where extractEnd = Map.foldrWithKey extractSubnets' extractStart graph
-        extractStart = seedProgress graph
-
-extractSubnets' :: Identifier -> NodeInfo -> ExtractionProgress -> ExtractionProgress
-extractSubnets' index node progress | Set.member index visited = progress
-                                    | otherwise                = progress'
-  where (Progress graph visited subs subsGraph) = progress
-        seed = fromNode (index :@ node)
+extractSubnets' :: CompositionGraph -> Identifier -> NodeInfo -> Extraction -> Extraction
+extractSubnets' graph index node accum@(subs, subsGraph)
+    | Map.member index subsGraph = accum
+    | otherwise                  = ((newsub:subs), subsGraph')
+  where seed = fromNode (index :@ node)
         newsub' = expandTentacle' seed    graph (Succ index)
         newsub  = expandTentacle' newsub' graph (Prem index)
-        visited' = Set.union visited (nodes newsub)
         subsGraph' = Set.foldr (flip Map.insert newsub) subsGraph (nodes newsub)
-        progress' = Progress graph visited' (newsub:subs) subsGraph'
 
 validExtensions :: SubnetGraph -> CompositionGraph -> Link -> Subnet -> [Subnet]
 validExtensions sgraph cgraph target net = case net of
